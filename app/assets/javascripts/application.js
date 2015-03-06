4333aeea66f4176389299e4f3637d01a403859db
Zach Meza <zachmmeza@gmail.com>
2015-03-06 02:49:30 -0800

    require publications in application.js?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,8 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
+//
+//= require publications
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
